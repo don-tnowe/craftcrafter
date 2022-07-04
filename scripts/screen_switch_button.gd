9529fc4f7af90084e8e85b11490eab00126d3d14
don-tnowe <67479453+don-tnowe@users.noreply.github.com>
2022-07-04 16:52:48 +0300

    Implement scene switch buttons

---
+++ scripts/screen_switch_button.gd
@@ -0,0 +1,38 @@
+extends Button
+
+export(String) var button_tag
+export(String, FILE, "*.tscn") var switch_to_screen
+
+var screen : Control
+var screen_switcher : Control
+
+
+func _ready() -> void:
+	var cur_parent = self
+	
+	while true:
+		cur_parent = cur_parent.get_parent()
+		if cur_parent.has_method("get_switch_extra_from_tag"):
+			screen = cur_parent
+			break
+
+		if cur_parent.has_method("switch_screen"):
+			screen_switcher = cur_parent
+			break
+
+		if !is_instance_valid(cur_parent):
+			return
+
+	if connect("pressed", self, "_on_pressed") != OK:
+		return
+
+
+func _on_pressed() -> void:
+	if !is_instance_valid(screen_switcher):
+		return
+
+	if !is_instance_valid(screen):
+		screen_switcher.switch_screen(switch_to_screen)
+
+	else:
+		screen_switcher.switch_screen(switch_to_screen, screen.get_switch_extra_from_tag(button_tag))
--- scripts/screen_switcher.gd
+++ scripts/screen_switcher.gd
@@ -1,12 +1,62 @@
 extends Control
 
 
-onready var node_base = $"base"
+onready var node_frame := $"frame"
+onready var node_anim := $"anim"
+
+var can_change_screen := true
 
 
-func _ready():
-	pass 
+func _ready() -> void:
+	$"header/color_rect/box/back".connect("pressed", self, "_on_back_pressed")
 
 
-func load_scene():
-	pass
+func switch_screen(scene_path : String, extra = null) -> void:
+	if !can_change_screen: return
+
+	var bottom_node = node_frame.get_child(node_frame.get_child_count() - 1)
+	var top_node = load(scene_path).instance()
+	
+	node_frame.add_child(top_node)
+	
+	if top_node.has_method("open_screen"):
+		top_node.open_screen(extra)
+	
+	start_animation("open_screen", str(get_path_to(bottom_node)), str(get_path_to(top_node)))
+		
+
+func _on_back_pressed() -> void:
+	if !can_change_screen: return
+	if node_frame.get_child_count() <= 2: return
+
+	var bottom_node = node_frame.get_child(node_frame.get_child_count() - 2)
+	var top_node = node_frame.get_child(node_frame.get_child_count() - 1)
+	
+	if top_node.has_method("close_screen"):
+		top_node.close_screen()
+	
+	if bottom_node.has_method("reopen_screen"):
+		if top_node.has_method("get_result"):
+			bottom_node.reopen_screen(top_node.get_result())
+			
+		else: 
+			bottom_node.reopen_screen()
+		
+	start_animation("close_screen", str(get_path_to(bottom_node)), str(get_path_to(top_node)))
+		
+
+func start_animation(name : String, bottom_node_path_str : String, top_node_path_str : String):
+	var animation = node_anim.get_animation(name)
+	animation.track_set_path(0, NodePath(bottom_node_path_str + ":modulate"))
+	animation.track_set_path(1, NodePath(bottom_node_path_str + ":visible"))
+	animation.track_set_path(2, NodePath(top_node_path_str + ":anchor_left"))
+	animation.track_set_path(3, NodePath(top_node_path_str + ":anchor_right"))
+
+	node_anim.play(name)
+	can_change_screen = false
+	
+
+func _on_anim_animation_finished(anim_name) -> void:
+	can_change_screen = true
+	if anim_name == "close_screen":
+		node_frame.get_child(node_frame.get_child_count() - 1).queue_free()
