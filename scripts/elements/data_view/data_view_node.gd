5ec940e2744e9ef94c3bcd3e0923cc963e74087d
don-tnowe <67479453+don-tnowe@users.noreply.github.com>
2022-07-12 17:50:02 +0300

    Entity edit screen layout

--- scripts/elements/data_view/data_view_node.gd
+++ scripts/elements/data_view/data_view_node.gd
@@ -4,12 +4,11 @@ export(String) var data_node_name
 export(bool) var data_node_name_from_parent_name
 export(bool) var start_from_root
 
-var data_path : String
 var data_collection # Variant: Array or Dictionary
 
 
 func _ready() -> void:
-	var cur_parent = get_parent()
+	var cur_parent = self
 	
 	if data_node_name_from_parent_name:
 		data_node_name = get_parent().name
@@ -17,22 +16,22 @@ func _ready() -> void:
 	while true:
 		cur_parent = cur_parent.get_parent()
 		if start_from_root || !is_instance_valid(cur_parent):
-			data_path = ""
-			data_collection = DataController.project_data[data_node_name]
-			break
-		
-		if cur_parent.has_node(@"data_view_node"):
-			var node = cur_parent.get_node(@"data_view_node")
-			data_path = node.data_path + "/" + data_node_name
-			if node.data_collection is Array:
-				data_collection = node.data_collection[data_node_name.to_int()]
-				
-			else:
-				data_collection = node.data_collection[data_node_name]
-				
+			data_collection = DataController.project_data
 			break
 			
-		if "screen_extra_data" in cur_parent:
-			data_collection = cur_parent.screen_extra_data[0][data_node_name]
+		if cur_parent.has_node(@"data_view_node"):
+			var node = cur_parent.get_node(@"data_view_node")
+			if node != self:
+				data_collection = node.data_collection
+				break
+			
+		if "screen_extra_data" in cur_parent && cur_parent.screen_extra_data != null:
+			data_collection = cur_parent.screen_extra_data[0]
 			break
-		
+			
+	if data_node_name != "":
+		if data_collection is Array:
+			data_collection = data_collection[data_node_name.to_int()]
+			
+		else:
+			data_collection = data_collection[data_node_name]
